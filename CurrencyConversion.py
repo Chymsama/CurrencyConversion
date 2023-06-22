bfebb9ee98134169ec664a813097054347a58418
Pham Anh Quang <quangpade170281@fpt.edu.vn>
2023-06-22 11:34:51 +0700

    Create CurrencyConversion.py

---
+++ CurrencyConversion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+usd=float(input('Nhập vào số USD cần đổi: '))
+r=float(input('Nhập vào tỉ giá USD/VND cần đổi: '))
+
+vnd= usd*r
+
+print('VND: ', vnd)
+
